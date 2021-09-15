42f4510372dcf631e9c8c9a0540232295b261ddc
VictorMachado38 <54145667+VictorMachado38@users.noreply.github.com>
2021-09-14 23:35:41 -0300

    Filter funcionando

--- AngularBasico/src/app/app.module.ts
+++ AngularBasico/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import {CourseListCompoment} from "./course-list.compoment";
+import {CourseListCompoment} from "./courses/course-list.compoment";
+import {StarComponet} from "./star/star.componet";
+import {CourseService} from "./courses/course.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListCompoment
+    CourseListCompoment,
+    StarComponet
+
   ],
   imports: [
     BrowserModule,
---
+++ AngularBasico/src/app/courses/course-list.compoment.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from "@angular/core";
+import {Course} from "./course";
+import {CourseService} from "./course.service";
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.html'
+
+})
+export class CourseListCompoment implements OnInit{
+
+  filterCourses: Course[] = [];
+
+  _couses: Course[] = [];
+
+  _filterby!: string;
+
+
+  constructor(private courseService: CourseService) {
+
+  }
+
+  ngOnInit(): void {
+    this._couses= this.courseService.retriveAll();
+    this.filterCourses = this._couses;
+
+  }
+
+  set filter(value: string){
+    this._filterby = value;
+
+    this.filterCourses = this._couses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterby.toLocaleLowerCase()) > -1)
+  }
+
+  get filter(){
+      return this._filterby;
+  }
+
+
+
+}
+
